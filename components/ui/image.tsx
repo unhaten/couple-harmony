8e6ab26a78ebb585f59807eff80a28db95326c44
unhaten <csgold2002@mail.ru>
2024-08-31 13:24:28 +0500

    setting things up

---
+++ components/ui/image.tsx
@@ -0,0 +1,16 @@
+import { default as NextImage } from 'next/image'
+
+import { AspectRatio } from '@/components/ui/aspect-ratio'
+
+export function Image() {
+	return (
+		<AspectRatio ratio={16 / 9} className='bg-muted'>
+			<NextImage
+				src='https://images.unsplash.com/photo-1588345921523-c2dcdb7f1dcd?w=800&dpr=2&q=80'
+				alt='Photo by Drew Beamer'
+				fill
+				className='h-full w-full rounded-md object-cover'
+			/>
+		</AspectRatio>
+	)
+}
